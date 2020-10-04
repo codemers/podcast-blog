a8ab608b9004f8293ac1c636371330f615d507ce
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2020-10-04 12:59:01 -0400

    Remove instagram page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,47 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import styled from 'styled-components'
-import { animated, useSpring, config } from 'react-spring'
-import Layout from '../components/layout'
-import GridItem from '../components/grid-item'
-import SEO from '../components/SEO'
-import { ChildImageSharp } from '../types'
+import React from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "styled-components";
+import { animated, useSpring, config } from "react-spring";
+import Layout from "../components/layout";
+import GridItem from "../components/grid-item";
+import SEO from "../components/SEO";
+import { ChildImageSharp } from "../types";
 
 type PageProps = {
   data: {
     firstProject: {
-      title: string
-      slug: string
-      cover: ChildImageSharp
-    }
+      title: string;
+      slug: string;
+      cover: ChildImageSharp;
+    };
     threeProjects: {
       nodes: {
-        title: string
-        slug: string
-        cover: ChildImageSharp
-      }[]
-    }
-    aboutUs: ChildImageSharp
-    instagram: ChildImageSharp
-  }
-}
+        title: string;
+        slug: string;
+        cover: ChildImageSharp;
+      }[];
+    };
+    aboutUs: ChildImageSharp;
+  };
+};
 
 const Area = styled(animated.div)`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: 35vw 40vw 25vw;
   grid-template-areas:
-    'first-project about-us about-us'
-    'three-projects three-projects three-projects'
-    'instagram instagram instagram';
+    "first-project about-us about-us"
+    "three-projects three-projects three-projects";
 
   @media (max-width: ${(props) => props.theme.breakpoints[3]}) {
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: 35vw 30vw 30vw 25vw;
 
     grid-template-areas:
-      'first-project first-project about-us about-us'
-      'three-projects three-projects three-projects three-projects'
-      'three-projects three-projects three-projects three-projects'
-      'instagram instagram instagram instagram';
+      "first-project first-project about-us about-us"
+      "three-projects three-projects three-projects three-projects"
+      "three-projects three-projects three-projects three-projects";
   }
 
   @media (max-width: ${(props) => props.theme.breakpoints[1]}) {
@@ -52,11 +49,10 @@ const Area = styled(animated.div)`
     grid-template-rows: repeat(5, 38vw);
 
     grid-template-areas:
-      'first-project about-us'
-      'three-projects three-projects'
-      'three-projects three-projects'
-      'three-projects three-projects'
-      'instagram instagram';
+      "first-project about-us"
+      "three-projects three-projects"
+      "three-projects three-projects"
+      "three-projects three-projects";
   }
 
   @media (max-width: ${(props) => props.theme.breakpoints[0]}) {
@@ -64,22 +60,21 @@ const Area = styled(animated.div)`
     grid-template-rows: repeat(6, 50vw);
 
     grid-template-areas:
-      'first-project'
-      'about-us'
-      'three-projects'
-      'three-projects'
-      'three-projects'
-      'instagram';
+      "first-project"
+      "about-us"
+      "three-projects"
+      "three-projects"
+      "three-projects";
   }
-`
+`;
 
 const FirstProject = styled(GridItem)`
   grid-area: first-project;
-`
+`;
 
 const AboutUs = styled(GridItem)`
   grid-area: about-us;
-`
+`;
 
 const ThreeProjects = styled.div`
   grid-area: three-projects;
@@ -90,24 +85,25 @@ const ThreeProjects = styled.div`
     grid-template-columns: 1fr;
     grid-template-rows: 1fr 1fr 1fr;
   }
-`
+`;
 
-const Instagram = styled(GridItem)`
-  grid-area: instagram;
-`
-
-const Index: React.FunctionComponent<PageProps> = ({ data: { firstProject, threeProjects, aboutUs, instagram } }) => {
+const Index: React.FunctionComponent<PageProps> = ({
+  data: { firstProject, threeProjects, aboutUs },
+}) => {
   const pageAnimation = useSpring({
     config: config.slow,
     from: { opacity: 0 },
     to: { opacity: 1 },
-  })
+  });
 
   return (
     <Layout>
       <SEO />
       <Area style={pageAnimation}>
-        <FirstProject to={firstProject.slug} aria-label={`View project "${firstProject.title}"`}>
+        <FirstProject
+          to={firstProject.slug}
+          aria-label={`View project "${firstProject.title}"`}
+        >
           <Img fluid={firstProject.cover.childImageSharp.fluid} />
           <span>{firstProject.title}</span>
         </FirstProject>
@@ -117,22 +113,22 @@ const Index: React.FunctionComponent<PageProps> = ({ data: { firstProject, three
         </AboutUs>
         <ThreeProjects>
           {threeProjects.nodes.map((project) => (
-            <GridItem to={project.slug} key={project.slug} aria-label={`View project "${project.title}"`}>
+            <GridItem
+              to={project.slug}
+              key={project.slug}
+              aria-label={`View project "${project.title}"`}
+            >
               <Img fluid={project.cover.childImageSharp.fluid} />
               <span>{project.title}</span>
             </GridItem>
           ))}
         </ThreeProjects>
-        <Instagram to="/instagram" aria-label="See my Instagram pictures">
-          <Img fluid={instagram.childImageSharp.fluid} />
-          <span>Instagram</span>
-        </Instagram>
       </Area>
     </Layout>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
 export const query = graphql`
   query Index {
@@ -160,19 +156,15 @@ export const query = graphql`
         }
       }
     }
-    aboutUs: file(sourceInstanceName: { eq: "images" }, name: { eq: "about-us" }) {
+    aboutUs: file(
+      sourceInstanceName: { eq: "images" }
+      name: { eq: "about-us" }
+    ) {
       childImageSharp {
         fluid(quality: 95, maxWidth: 1200) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
-    instagram: file(sourceInstanceName: { eq: "images" }, name: { eq: "instagram" }) {
-      childImageSharp {
-        fluid(quality: 95, maxWidth: 1920) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
   }
-`
+`;
